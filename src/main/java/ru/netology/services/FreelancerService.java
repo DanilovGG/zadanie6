d1e954a474e867bb29bb7a722f06d9d7db4ca688
DanilovGG <derzhik95@gmail.com>
2024-09-06 18:18:36 +0300

    Initial commit

--- src/main/java/ru/netology/services/FreelancerService.java
+++ src/main/java/ru/netology/services/FreelancerService.java
@@ -7,25 +7,17 @@ public class FreelancerService {
         int money = 0;
 
         for (int month = 0; month < 12; month++) {
+            System.out.print("Месяц " + (month + 1) + ". Денег " + money + ". " + (money >= threshold ? "Буду отдыхать." : "Придётся работать."));
             if (money >= threshold) {
                 count++;
                 money -= expenses; // обязательные месячные траты
                 money /= 3; // траты на отдых
+                System.out.println("Потратил -" + expenses + ", затем ещё -" + (money * 3 * 2 / 3));
             } else {
                 money += income;
                 money -= expenses; // обязательные месячные траты
-            } if (month == 0){
-                money = 0; // в первый месяц денег нет
-            }
-
-            // Вывод эмуляции планируемого поведения
-            System.out.print("Месяц " + (month + 1) + ". Денег " + money + ". " + (money >= threshold ? "Буду отдыхать." : "Придётся работать."));
-            if (money >= threshold) {
-                System.out.println("Потратил -" + expenses + ", затем ещё -" + ((money - expenses) * 2 / 3));
-            } else {
                 System.out.println("Заработал +" + income + ", потратил -" + expenses);
             }
-
         }
         System.out.println();
         // Возвращаем количество месяцев отдыха
